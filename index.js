23b64b69ce3725d1201dc4dbb9d473929147a590
Diluk Mihiranga <dilukedu@gmail.com>
2024-10-24 09:52:06 +0530

    Controllers changed

--- index.js
+++ index.js
@@ -1,36 +1,47 @@
-const express = require("express");
-const authRoute = require("./routes/auth.Routes");
-const connection = require("./utils/DBConnection");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+import express from "express";
+import bodyParser from "body-parser";
 
+import cors from "cors";
+
+import authRoutes from "./routes/auth.js";
+import connection from "./utils/DBConnection.js";
+
+/* CONFIGURATION */
 const app = express();
-
-require("dotenv").config();
-
 app.use(cors());
-// app.use(express.json());
-// // Parse URL-encoded and JSON data
-// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/abc", (req, res) => {
-  res.send("Hello, abc!");
-});
+/* ROUTES */
 
-app.use("/api/auth", authRoute);
-app.use("/", (req, res) => {
-  res.send("Hello, World!");
-});
+app.use("/api/auth", authRoutes);
 
-app.listen(3000, () => {
-  connection.connect(function (err) {
-    if (err) {
-      console.error("Error connecting to MySQL: " + err.stack);
-      return;
-    }
+/* MYSQL SETUP */
+const PORT = 5000 || 9000;
+async function startServer() {
+  try {
+    connection.connect(function (err) {
+      if (err) {
+        console.error("Error connecting to MySQL: " + err.stack);
+        return;
+      }
 
-    console.log("Connected to MySQL as ID " + connection.threadId);
-  });
-  console.log("Server listening on port 3000");
-});
+      console.log("Connected to MySQL as ID " + connection.threadId);
+    });
+
+    app.get("/", (req, res) => {
+      res.send("Hello World!");
+    });
+
+    // Start the server
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+  } catch (error) {
+    console.error("Database connection failed:", error);
+  }
+}
+
+// Call the async function to start the server
+startServer();
